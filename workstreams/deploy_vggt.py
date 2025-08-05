2c87b1ffaeee3f43c1d52be83416cd536e89c9a6
Alex Carter <victoryforphil@gmail.com>
2025-08-05 00:00:55 -0700

    Prefect task

--- workstreams/deploy_vggt.py
+++ workstreams/deploy_vggt.py
@@ -1,8 +1,10 @@
+
 from prefect import flow
 from prefect.docker import DockerImage
 from vggt_s3_task import vggt_process_images_from_s3
 
 if __name__ == "__main__":
+    # Deploy the VGGT image processing workflow
     vggt_process_images_from_s3.deploy(
         name="vggt-s3-processor",
         work_pool_name="my-docker-pool",
@@ -11,7 +13,13 @@ if __name__ == "__main__":
             # tag="latest",
             dockerfile="test.Dockerfile"
         ),
-        job_variables={"network_mode": "host"},
+        job_variables={
+            "network_mode": "host",
+            "env": {
+                "PREFECT_LOGGING_LEVEL": "INFO"
+            }
+        },
         push=False,
-        build=True
+        build=True,
+        description="Process images from S3 with VGGT and save 3D results back to S3",
     )
